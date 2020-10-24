df208516ee56a86f2c92133965e319291f93a6de
Tim Buckland <tim.buckland@gmail.com>
2020-10-24 19:13:24 +0100

    challenge 2 was both harder and easier than expected

--- challenges/cypress/integration/challenge_02.js
+++ challenges/cypress/integration/challenge_02.js
@@ -3,11 +3,11 @@ describe('Totaljobs http Sign In', function () {
 
         let postbody = 'Form.Email=example_jobseeker@example.com&Form.Password=example1&Form.RememberMe=true&Form.RememberMe=false'
 
-        let url = 'https://www.totaljobs.com/account/signin'
+        let signin_url = 'https://www.totaljobs.com/account/signin'
 
         cy.request({
             method: 'POST',
-            url: url,
+            url: signin_url,
             followRedirect: false,
             form: true,
             body: postbody
@@ -15,13 +15,17 @@ describe('Totaljobs http Sign In', function () {
             expect(response.status).to.eq(302)
         } )
 
+        let account_url = 'https://www.totaljobs.com/Authenticated/Default.aspx'
+
         cy.request({
             method: 'GET',
-            url: 'https://www.totaljobs.com/Authenticated/Default.aspx',
+            url: account_url,
         }).then((response) => {
             expect(response.status).to.eq(200)
             expect(response.body).to.contain('Welcome back')
         } )
+
+        cy.visit(account_url);
 
     })
 })
