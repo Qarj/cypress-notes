5e91e9576bdc94dc84cf40bbd35bd73aa1b1994c
Tim Buckland <tim.buckland@gmail.com>
2020-10-24 11:25:48 +0100

    got working

--- challenges/challenge_01/cypress/integration/challenge_01.js
+++ challenges/challenge_01/cypress/integration/challenge_01.js
@@ -1,9 +1,17 @@
 describe('Totaljobs Sign In', function () {
     it('Signs into Totaljobs', function () {
-        cy.visit('https://www.totaljobs.com')
+        cy.visit('https://www.totaljobs.com');
         
         cy.get('.accept-button-new').click();
 
-        cy.get('[data-target="#navbar-desktop-signin-links"]').click()
+        cy.get('[data-target="#navbar-desktop-signin-links"]').click();
+
+        cy.contains('Jobseeker sign in').click();
+
+        cy.get('[name="Form.Email"]').type('example_jobseeker@example.com');
+
+        cy.get('[name="Form.Password"]').type('example1');
+
+        cy.get('[value="Sign in"]').click();
     })
 })
